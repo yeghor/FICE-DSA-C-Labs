57c0afacb1281fec1f9eed4efb6a257db2f4148f
RomanovYehor <yeghor.romanov@gmail.com>
2025-12-03 23:41:28 +0200

    added lab-5 code

--- lab-4/src/task.c
+++ lab-4/src/task.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 // Matrix first row and last col most left element binary search implementation
---
+++ lab-5/src/task.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define ROWS 10
+#define COLS 10
+
+void createMatrix(bool ascending, int instances, int rows, int cols, int matrix[rows][cols]) {
+    int value;
+    if (!ascending) {
+        value = rows * cols;
+    } else {
+        value = 0;
+    }
+
+    if (instances < 1) {
+        instances = 1;
+    }
+
+    int instancesCounter = 0;
+
+    // Create matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = value;
+
+            instancesCounter++;
+
+            if (instancesCounter >= instances || instances == 1) {
+                instancesCounter = 0;
+                if (!ascending) {
+                    value -= 1;
+                } else {
+                    value++;
+                }
+            }
+        }
+    }
+}
+
+void displayMatrix(int matrix[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%3d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[ROWS][COLS];
+
+    createMatrix(true, 1, ROWS, COLS, matrix);
+
+    displayMatrix(matrix);
+
+    int i, j;
+    int secondIterations = COLS-1;
+    int firstIterations = COLS-1;
+
+    for (int iterations = 0; iterations < abs(COLS); iterations++ ) {
+        i = 0;
+        j = 0;
+
+        // _ < COLS || _ < ROWS - Doesn't matter because input is square matrix
+        for (int _ = 0; _ < firstIterations; _++) {
+            if (matrix[i][j] < matrix[i+1][j+1]) {
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[i+1][j+1];
+                matrix[i+1][j+1] = temp;
+            }
+            i++;
+            j++;
+        }
+
+        firstIterations -= 1;
+
+        i = ROWS-1;
+        j = COLS-1;
+
+        // _ < COLS || _ < ROWS - Doesn't matter because input is square matrix
+        for (int _ = 0; _ < secondIterations; _++) {
+            if (matrix[i][j] > matrix[i-1][j-1]) {
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[i-1][j-1];
+                matrix[i-1][j-1] = temp;
+            }
+            i -= 1;
+            j -= 1;
+        }
+
+        secondIterations -= 1;
+    }
+
+    printf("\n\n");
+    displayMatrix(matrix);
+}
